a5537963c05a764f64868853905a537283f66050
Nithila Thawalampitiya <nithilanimraka@gmail.com>
2025-06-03 23:39:59 +0530

    Refactor: Update API key handling and enhance error handling with dependency analysis

--- src/code_review/llm_utils.py
+++ src/code_review/llm_utils.py
@@ -22,13 +22,26 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # if not groq_api_key:
 #     raise ValueError("GROQ_API_KEY is not set")
 
-google1_api_key = os.environ.get('GOOGLE_API_KEY')
+google1_api_key = os.environ.get('GEMINI_API_REVIEW_KEY')
 if not google1_api_key:
     raise ValueError("GOOGLE_API_KEY is not set")
 
 gemini_api_key = os.environ.get('GEMINI_API_KEY')
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set")
+
+
+gemini_api_key2 = os.environ.get('GOOGLE_API_KEY')
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set")
+
+nithila_api_key = os.environ.get('NITHILA_GOOGLE_API_KEY')
+if not nithila_api_key:
+    raise ValueError("NITHILA_API_KEY is not set")
+
+randinu_api_key = os.environ.get('RANDINU_GOOGLE_API_KEY')
+if not randinu_api_key:
+    raise ValueError("RANDINU_API_KEY is not set")
 
 llm_gemini = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
@@ -38,10 +51,41 @@ llm_gemini = ChatGoogleGenerativeAI(
     timeout=None,
     max_retries=2,
 )
+llm_gemini2 = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=gemini_api_key2,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
 
-llm_fin_gemini = ChatGoogleGenerativeAI(model="gemini-2.0-flash",
-                        google_api_key=google1_api_key,
-                         temperature=0)
+llm_nithila = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=nithila_api_key,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+llm_randinu = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=randinu_api_key,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+llm_fin_gemini = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=nithila_api_key,
+    temperature=0,
+    max_tokens=None,
+    timeout=60,
+    max_retries=2,
+    )
 
 
 # Schema for structured output to use in planning
@@ -86,6 +130,11 @@ class ReviewDatas(BaseModel):
     reviewDatas: List[ReviewData] = Field(description="Reviews of Data of the Code.",)
 
 structured_llm = llm_gemini.with_structured_output(ReviewDatas)
+
+structured_llm2 = llm_gemini2.with_structured_output(ReviewDatas)
+
+structured_llm_nithila = llm_nithila.with_structured_output(ReviewDatas)
+structured_llm_randinu = llm_randinu.with_structured_output(ReviewDatas)
 
 def line_numbers_handle(start_line_with_prefix, end_line_with_prefix):
     value1 = start_line_with_prefix
@@ -155,7 +204,7 @@ def security_handle(state: State):
      messages = prompt_templates.security_prompt.format_messages(PR_data=state["PR_data"])
      try:
          logging.info("Invoking LLM for security handling...")
-         response = structured_llm.invoke(messages)
+         response = structured_llm_nithila.invoke(messages)
          print("Security handling response: \n",response)
          print("\n\n")
          if(response == None):
@@ -175,7 +224,7 @@ def performance_handle(state: State):
      messages = prompt_templates.performance_prompt.format_messages(PR_data=state["PR_data"])
      try:
          logging.info("Invoking LLM for performance handling...")
-         response = structured_llm.invoke(messages)
+         response = structured_llm_randinu.invoke(messages)
          print("Performance handling response: \n",response)
          print("\n\n")
          if(response == None):
@@ -195,7 +244,7 @@ def quality_handle(state: State):
      messages = prompt_templates.quality_prompt.format_messages(PR_data=state["PR_data"])
      try:
          logging.info("Invoking LLM for quality handling...")
-         response = structured_llm.invoke(messages)
+         response = structured_llm_randinu.invoke(messages)
          print("Quality handling response: \n",response)
          print("\n\n")
          if(response == None):
@@ -346,6 +395,7 @@ def final_review(pr_data:str) -> List[Dict]:
     print("------------------------")
 
     final_response= structured_gemini_llm.invoke(final_prompt.format_messages(PR_data=pr_data, Issues=final_issues))
+    print("done final review")
 
     return [review.model_dump() for review in final_response.finalReviews]
 
